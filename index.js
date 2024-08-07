1eb14e5e1da8d169a1e07e857bb7615449ea2841
USAMA RAMZAN <104649305+usamaramzan47@users.noreply.github.com>
2024-08-07 10:22:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '/client/build')))
 
 //render client for any path 
 app.get('*', (req, res) => res.sendFile((path.join(__dirname, '/client/build/index.html'))));
-
+app.post('*', (req, res) => res.sendFile((path.join(__dirname, '/client/build/index.html'))));
 // Connect to the database
 connectDB();
 
